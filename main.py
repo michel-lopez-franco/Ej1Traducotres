62a0fb021fdbffe5fa91f773726ce8ad25061683
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2025-01-20 13:30:59 -0600

    segundo camibo

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("hola mundo")
+
+suma = 2 + 2
+
+def suma(a, b):
+    return a + b
+
+    def valida_cadena(cadena):
+        if isinstance(cadena, str):
+            return True
+        return False
+    
+    
+print("hola dos")
